db04e2e783e04800dc8584181393d3dab6058a1c
s1f10240241 <s1f102402417@iniad.org>
2024-09-12 19:07:28 +0900

    ほぼ完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import styles from './index.module.css';
 
-const directions = [
+const DIRECTIONS = [
   [0, -1],
   [1, -1],
   [1, 0],
@@ -12,35 +12,90 @@ const directions = [
   [-1, -1],
 ];
 
+const checkPutable = (cx: number, cy: number, board: number[][], turn: number) => {
+  if (board[cy][cx] === 1 || board[cy][cx] === 2) {
+    return;
+  }
+  for (const direction of DIRECTIONS) {
+    const dx = direction[0];
+    const dy = direction[1];
+    if (board[cy + dy] === undefined) continue;
+    if (board[cy + dy][cx + dx] === 3 - turn) {
+      for (let distance = 2; distance < 8; distance++) {
+        if (board[cy + dy * distance] === undefined) break;
+        if (board[cy + dy * distance][cx + dx * distance] === turn) {
+          return true;
+        }
+      }
+    }
+  }
+};
+
 const Home = () => {
   const [board, setBoard] = useState([
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,1,2,0,0,0],
-    [0,0,0,2,1,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 3, 0, 0, 0],
+    [0, 0, 0, 1, 2, 3, 0, 0],
+    [0, 0, 3, 2, 1, 0, 0, 0],
+    [0, 0, 0, 3, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
   ]);
 
-  const handleOnClick = (x: number, y:number) => {
+  const [turn, setTurn] = useState(1);
+
+  const handleOnClick = (x: number, y: number) => {
+    if (board[y][x] !== 0 && board[y][x] !== 3) {
+      return;
+    }
+
     const newBoard = structuredClone(board);
-    newBoard[y][x] = 1;
-    setBoard(newBoard);
-  }
+    for (const direction of DIRECTIONS) {
+      const dx = direction[0];
+      const dy = direction[1];
+      if (newBoard[y + dy] === undefined) continue;
+      if (newBoard[y + dy][x + dx] === 3 - turn) {
+        for (let distance = 2; distance < 8; distance++) {
+          if (newBoard[y + dy * distance] === undefined) break;
+          if (newBoard[y + dy * distance][x + dx * distance] === turn) {
+            newBoard[y][x] = turn;
+
+            for (let i = distance; i > 0; i--) {
+              newBoard[y + dy * i][x + dx * i] = turn;
+            }
+            for (let cy = 0; cy < 8; cy++) {
+              for (let cx = 0; cx < 8; cx++) {
+                if (newBoard[cy][cx] === 3) newBoard[cy][cx] = 0;
+                if (checkPutable(cx, cy, newBoard, 3 - turn)) {
+                  newBoard[cy][cx] = 3;
+                }
+              }
+            }
+            setBoard(newBoard);
+            setTurn(3 - turn);
+          }
+        }
+      }
+    }
+  };
 
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-        {board.map((row,y) =>
-        row.map((color,x) => (
-          <div
-            key={'${x}-${y}'}
-            className={styles.cell}
-            onClick={() => handleOnClick(x,y)}
-          >
-            <div className = {styles.stone} style={{backgroundColor: color === 1 ? 'black' : color === 2 ? 'white' :''}} /></div>
+        {board.map((row, y) =>
+          row.map((color, x) => (
+            <div key={'${x}-${y}'} className={styles.cell} onClick={() => handleOnClick(x, y)}>
+              <div
+                className={styles.stone}
+                style={{
+                  backgroundColor:
+                    color === 1 ? 'black' : color === 2 ? 'white' : color === 3 ? 'pink' : '',
+                  width: color === 3 ? '20%' : '70%',
+                  height: color === 3 ? '20%' : '70%',
+                }}
+              />
+            </div>
           )),
         )}
       </div>
